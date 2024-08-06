35351fbdfc48b3a2ce178276856407634539e4d3
isabellaterano <isabellaiterano@gmail.com>
2024-08-05 21:39:25 -0300

    changed the translation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Stars } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
 import { useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
 import {
   useMotionTemplate,
   useMotionValue,
@@ -13,6 +13,7 @@ import { useTranslations } from "next-intl";
 import { Link } from "@/navigation";
 import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 
+// Constants
 const COLORS_TOP: string[] = ["#CBF1F5", "#A6E3E9", "#71C9CE", "#00ADB5"];
 
 const HomePage: React.FC = () => {
@@ -26,7 +27,7 @@ const HomePage: React.FC = () => {
       repeat: Infinity,
       repeatType: "mirror",
     });
-  }, [color]);
+  }, [color, COLORS_TOP]);
 
   const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #0a0a0a 50%, ${color})`;
   const border = useMotionTemplate`1px solid ${color}`;
@@ -34,56 +35,30 @@ const HomePage: React.FC = () => {
 
   return (
     <motion.section
-      style={{
-        backgroundImage,
-      }}
+      style={{ backgroundImage }}
       className="relative grid min-h-screen place-content-center overflow-hidden bg-slate-950 text-slate-200 pt-16"
     >
       <div className="relative z-10 flex flex-col items-center">
-        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-5xl font-medium leading-tight text-transparent sm:text-6xl sm:leading-tight md:text-7xl md:leading-tight">
+        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight">
           {t("h1")}
         </h1>
-        <p className="mt-2 mb-8 max-w-xl text-center text-lg leading-relaxed md:text-lg md:leading-relaxed">
-          {t("p")}
-        </p>
-        <div className="flex">
-          <motion.div
-            style={{
-              border,
-              boxShadow,
-            }}
-            whileHover={{
-              scale: 1.015,
-            }}
-            whileTap={{
-              scale: 0.985,
-            }}
-            className="group relative flex justify-center w-fit items-center gap-1.5 rounded-md bg-gray-950/10 px-4 py-2 mr-2 text-gray-50 transition-colors hover:bg-gray-950/50"
-          >
-            <Link href="/projects" className="inline-flex items-center gap-1.5">
-              see my projects{" "}
-              <ArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
-            </Link>
-          </motion.div>
+        <p className="my-6  text-center text-base md:text-2xl">{t("p")}</p>
 
-          <motion.div
-            style={{
-              border,
-              boxShadow,
-            }}
-            whileHover={{
-              scale: 1.015,
-            }}
-            whileTap={{
-              scale: 0.985,
-            }}
-            className="group relative flex justify-center w-fit items-center gap-1.5 rounded-md bg-gray-950/10 px-4 py-2 mr-2 text-slate-50 transition-colors hover:bg-gray-950/50"
-          >
-            <Link href="/contact" className="inline-flex items-center gap-1.5">
-              contact me
-              <ArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
-            </Link>
-          </motion.div>
+        <div className="flex mt-6">
+          {["/projects", "/contact"].map((href, index) => (
+            <motion.div
+              key={href}
+              style={{ border, boxShadow }}
+              whileHover={{ scale: 1.015 }}
+              whileTap={{ scale: 0.985 }}
+              className="group relative flex justify-center w-fit items-center gap-1.5 rounded-md bg-gray-950/10 px-4 py-2 mx-2 text-slate-50 transition-colors hover:bg-gray-950/50"
+            >
+              <Link href={href} className="inline-flex items-center gap-1.5">
+                {t(index === 0 ? "button" : "button2")}
+                <ArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+              </Link>
+            </motion.div>
+          ))}
         </div>
       </div>
 
