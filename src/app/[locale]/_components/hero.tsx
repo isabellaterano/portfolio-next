cc3c0320bdf1527579bed7ec580ad1726d69c416
isabellaterano <isabellaiterano@gmail.com>
2024-08-19 19:38:10 -0300

    update link

--- src/app/[locale]/_components/hero.tsx
+++ src/app/[locale]/_components/hero.tsx
@@ -87,10 +87,10 @@ const Hero: React.FC = () => {
             whileTap={{ scale: 0.985 }}
             className="group relative flex items-center gap-1.5 rounded-md bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
           >
-            <Link href="#contact" className="inline-flex items-center gap-1.5">
+            <a href="#contact" className="inline-flex items-center gap-1.5">
               {t("button2")}
               <ArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
-            </Link>
+            </a>
           </motion.div>
         </div>
       </div>
